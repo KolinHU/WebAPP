ae2bd846045137ae866f58a9a8af11f6b33c1297
kolin <hlc246@126.com>
2018-03-20 17:04:07 +0800

    添加ORM,MODEL文件

---
+++ www/models.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Models for user, blog, comment.
+'''
+
+__author__ = 'Michael Liao'
+
+import time, uuid
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+import orm
+from models import User,Blog,Comment
+
+loop = asyncio.get_event_loop()
+async def test():
+    #创建连接池,里面的host,port,user,password需要替换为自己数据库的信息
+    await orm.create_pool(loop=loop, host=' 192.168.43.50', port=3306, user='root', password='123456',db='awesome')
+    #没有设置默认值的一个都不能少
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank',id="123")
+    await u.save()
+
+# 把协程丢到事件循环中执行
+loop.run_until_complete(test())
